b4f6d0b56b7be43e22388d6dc1b1ef401d75b662
web-altaf-1 <web.altaf.1@gmail.com>
2022-07-17 20:52:18 +0600

    all initial setup added

---
+++ src/components/RequireAuth/RequireAuth.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const RequireAuth = () => {
+    return (
+        <div>
+            <h2>This is RequireAuth</h2>
+        </div>
+    );
+};
+
+export default RequireAuth;
